ced86cab86f78f30b210070786771022ec02d590
Your Name <golamsarwar1896@gmail.com>
2024-11-11 23:34:43 +0600

    project setup done. working with login page

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-5xl text-center">HOME</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB98Ko_tI11NmFg_mlX3PbC1v0UE9jnfkQ",
+  authDomain: "login-registration-fireb-297cb.firebaseapp.com",
+  projectId: "login-registration-fireb-297cb",
+  storageBucket: "login-registration-fireb-297cb.firebasestorage.app",
+  messagingSenderId: "840398810640",
+  appId: "1:840398810640:web:df224dce5477f1b418a495",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
